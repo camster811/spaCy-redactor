0753f840bc6b16b56845ac2d792473fc1c7bea00
Camster811 <cls0150@auburn.edu>
2025-05-19 01:07:43 -0500

    implement logger

--- app/logger.py
+++ app/logger.py
@@ -1,5 +1,22 @@
+import logging
+
 # Set up basic logger
 # - Add timestamps, log levels
 # - Optionally log to file
 
 # Use logger across main.py, redact.py, etc.
+# Configure logger
+logger = logging.getLogger("spacy_redactor")
+logger.setLevel(logging.INFO)
+
+formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(name)s - %(message)s")
+
+# Console handler
+ch = logging.StreamHandler()
+ch.setFormatter(formatter)
+logger.addHandler(ch)
+
+# Optional: File handler (uncomment to enable file logging)
+fh = logging.FileHandler("spacy_redactor.log")
+fh.setFormatter(formatter)
+logger.addHandler(fh)
